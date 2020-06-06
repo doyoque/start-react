e90a2bf3bc1073ebaa440ae5b650677cc2bacf77
yoga <abiyoga1335@gmail.com>
2020-06-06 16:43:20 +0700

    add user

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { useState } from 'react';
 import './App.css';
-import UserTable from './tables/UserTable'
+import UserTable from './tables/UserTable';
+import AddUserForm from './forms/AddUserForm';
 
 const App = () => {
   const usersData = [
     { id: 1, name: 'Doyoque', username: 'floppyDisk' },
     { id: 2, name: 'Tania', username: 'hardDisk' },
     { id: 3, name: 'Gerard', username: 'flashDisk' }
-  ];
+  ]
 
   // const initialForm = { id: null, name: '', username: '' }
 
   //set state
-  const [users] = useState(usersData)
+  const [users, setUser] = useState(usersData)
+
+  //CRUD operation
+  const addUser = user => {
+    user.id = users.length + 1
+    setUser([ ...users, user ])
+  }
 
   var dom = 
     <div className="container">
@@ -20,6 +27,7 @@ const App = () => {
       <div className="flex-row">
         <div className="flex-large">
           <h2>Add user</h2>
+          <AddUserForm addUser={addUser} />
         </div>
         <div className="flex-large">
           <h2>View users</h2>
@@ -27,6 +35,7 @@ const App = () => {
         </div>
       </div>
     </div>
+    
   return (
     dom
   );
--- src/tables/UserTable.js
+++ src/tables/UserTable.js
@@ -1,39 +1,33 @@
 import React from 'react'
 
-const UserTable = props => {
+const UserTable = props => (
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Username</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      {props.users.length > 0 ? (
+        props.users.map(user => (
+          <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.username}</td>
+            <td>
+              <button className="button muted-button">Edit</button>
+              <button className="button muted-button">Delete</button>
+            </td>
+          </tr>
+        ))
+      ) : (
+        <tr>
+          <td colSpan={3}>No users</td>
+        </tr>
+      )}
+    </tbody>
+  </table>
+)
 
-	var table = 
-	  <table>
-	    <thead>
-	      <tr>
-	        <th>Name</th>
-	        <th>Username</th>
-	        <th>Actions</th>
-	      </tr>
-	    </thead>
-	    <tbody>
-	    	{props.users.length > 0 ? (
-	    		props.users.map(user => (
-			      <tr>
-			        <td>{user.name}</td>
-			        <td>{user.username}</td>
-			        <td>
-			          <button className="button muted-button">Edit</button>
-			          <button className="button muted-button">Delete</button>
-			        </td>
-			      </tr>
-	    			))
-	    		) : (
-	    			<tr>
-	    				<td colSpan={3}>No Users</td>
-	    			</tr>
-	    		)}
-	    </tbody>
-	  </table>
-
-	return (
-		table
-	);
-}
-
-export default UserTable;
+export default UserTable
